f61a7aaa97915b0d474366504710e68c87d1316d
erru-2005 <errualmeida2005@gmail.com>
2025-08-12 12:53:16 +0530

    pushing code from clg pc

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\inaugration\library_Mgt_BBHC\venv"
+set "VIRTUAL_ENV=D:\inaugration\virtual-env\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
